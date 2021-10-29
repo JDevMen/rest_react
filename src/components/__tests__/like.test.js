50c2bbfad423c308743296543b580401a67738fc
JDevMen <jdmendoza_90@hotmail.com>
2021-10-29 14:01:10 -0500

    tests de incremento y decremento

--- src/components/__tests__/like.test.js
+++ src/components/__tests__/like.test.js
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
 import Like from "../like";
@@ -18,8 +19,26 @@ afterEach(() => {
 });
 
 describe("Testing Like component", () => {
-  it("Like counter is 'Likes: 0' by default ", () => {
+  it("Like counter is 'Likes: 0' by default", () => {
     const p = container.querySelector("p");
     expect(p.textContent).toBe("Likes: 0");
+  });
+
+  it("Likes increases after clicking Like button", () => {
+    const p = container.querySelector("p");
+    const likeButton = container.querySelector("#increment");
+    act(() => {
+      likeButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+    expect(p.textContent).toBe("Likes: 1");
+  });
+
+  it("Likes decreases after clicking Like button", () => {
+    const p = container.querySelector("p");
+    const likeButton = container.querySelector("#decrement");
+    act(() => {
+      likeButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+    expect(p.textContent).toBe("Likes: -1");
   });
 });
